c2c6a40d3863ca50b246ee9d160593f203106902
Rachel Burkhardt <r.burkhardt.ibclc@gmail.com>
2024-09-09 20:57:33 -0500

    Exercises have been completed

--- control-flow-and-collections/exercises/untitled/src/main/java/org/example/HashMapPractice.java
+++ control-flow-and-collections/exercises/untitled/src/main/java/org/example/HashMapPractice.java
@@ -1,4 +1,39 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class HashMapPractice {
+    public static void main(String[] args) {
+        HashMap<String, Integer> students = new HashMap<>();
+        Scanner input = new Scanner(System.in);
+        String newStudent;
+
+        //This is just telling the user what they will need to do
+        System.out.println("Enter your students name (or ENTER to finish : ");
+
+        //Get student names and ID numbers
+        do {
+            System.out.println("Student: ");
+            newStudent = input.nextLine();
+
+            if(!newStudent.equals("")) {
+                System.out.println("ID Number: ");
+                Integer idNum = input.nextInt();
+                students.put(newStudent, idNum);
+
+                //Read in the newline before looping back since they next input is a string
+                input.nextLine();
+            }
+
+        } while (!newStudent.equals(""));
+
+        //Print the student and ID numbers
+        System.out.println("\nStudent and ID numbers:");
+
+        for (Map.Entry<String, Integer> student : students.entrySet()) {
+            System.out.println((student.getKey() + ": " + student.getValue()));
+        }
+    }
 }
--- control-flow-and-collections/exercises/untitled/src/main/java/org/example/fiveLetterWords.java
+++ control-flow-and-collections/exercises/untitled/src/main/java/org/example/fiveLetterWords.java
@@ -8,20 +8,15 @@ public class fiveLetterWords {
 
         //PROMPT THE USER FOR A WORD
         Scanner input = new Scanner(System.in);
-        boolean found;
+
 
         System.out.println("Enter the word length we should look for: ");
         int numChars = input.nextInt();
 
         for (String word : words) {
-            if(word.length() == numChars){
+            if (word.length() == numChars) {
                 System.out.println(word);
-                found = true;
-
-            } else if (!found){
-                System.out.println("There are no words with that numbers of letters.");
             }
-
         }
 
     }
