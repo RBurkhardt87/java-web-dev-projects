7193a8ac2a8a72b6ee2fc545050bcfbf6d14dcba
Rachel Burkhardt <r.burkhardt.ibclc@gmail.com>
2024-09-11 15:56:57 -0500

    Complete

--- control-flow-and-collections/studio/counting-chracters/src/main/java/org/example/Main.java
+++ control-flow-and-collections/studio/counting-chracters/src/main/java/org/example/Main.java
@@ -3,20 +3,32 @@ package org.example;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String quote = "If the product of two terms is zero " +
-                "then common sense says at" + " least one of the two terms has to be" + " zero to start with. " +
-                "So if you move all the" + " terms over to one side, you can put the quadratics into a form that can" +
-                " be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty " +
-                "straightforward from there.";
+//        String quote = "If the product of two terms is zero " +
+//                "then common sense says at" + " least one of the two terms has to be" + " zero to start with. " +
+//                "So if you move all the" + " terms over to one side, you can put the quadratics into a form that can" +
+//                " be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty " +
+//                "straightforward from there.";
 
-        char[] charactersInString = quote.toCharArray();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a quote: ");
+        String quote = input.nextLine();
+
+
+        //Making sure that there are no non-alpha letters in our HashMap
+        //First, declare a variable with String type and assign it to the alphabet.
+        String alphabet ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        char[] charactersInString = quote.toUpperCase().toCharArray();
 
         HashMap<Character, Integer> charactersMap = new HashMap<>();
 
         for (char letter : charactersInString) {
+
+
             if (charactersMap.containsKey(letter)) {
                 charactersMap.put(letter, charactersMap.get(letter) + 1);
             } else {
@@ -28,6 +40,7 @@ public class Main {
         for (Map.Entry<Character, Integer> letter : charactersMap.entrySet()) {
             System.out.println(letter.getKey() + ": " + letter.getValue());
 
+            input.close();
         }
     }
 }
