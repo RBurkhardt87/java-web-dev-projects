48edb251f32eee484657f89327e9e8b55fb681d4
Rachel Burkhardt <r.burkhardt.ibclc@gmail.com>
2024-09-18 13:19:48 -0500

    I believe the exercise is now complete

--- classes-part-2/exercises/src/main/java/org/launchcode/Course.java
+++ classes-part-2/exercises/src/main/java/org/launchcode/Course.java
@@ -7,11 +7,62 @@ public class Course {
     private Teacher instructor;
     private ArrayList<Student> enrolledStudents;
 
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public Teacher getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Teacher instructor) {
+        this.instructor = instructor;
+    }
+
+    public ArrayList<Student> getEnrolledStudents() {
+        return enrolledStudents;
+    }
+
+    public void setEnrolledStudents(ArrayList<Student> enrolledStudents) {
+        this.enrolledStudents = enrolledStudents;
+    }
+
+
     // TODO: Add your custom 'toString' method here. Make sure it returns a well-formatted String rather than
     //  just the class fields.
+
+    @Override
+    public String toString(){
+        return "The enrolledStudents for this course are " + enrolledStudents + ". The course instructor will be : " +
+                instructor + " and they will cover " + topic + ".";
+
+    }
 
 
     // TODO: Add your custom 'equals' method here. Consider which fields should match in order to call two
     //  Course objects equal.
 
+    @Override
+    public boolean equals(Object toBeCompared){
+
+        if(toBeCompared == this){
+            return true;
+        }
+        if(toBeCompared == null){
+            return false;
+        }
+        if(toBeCompared.getClass() != getClass()){
+            return false;
+        }
+        Course theCourse = (Course) toBeCompared;
+        return theCourse.getInstructor() == getInstructor();
+    }
+
+
 }
+
+
--- classes-part-2/exercises/src/main/java/org/launchcode/Student.java
+++ classes-part-2/exercises/src/main/java/org/launchcode/Student.java
@@ -30,20 +30,58 @@ public class Student {
 
 
     //TODO: Uncomment and complete the getGradeLevel method here:
-//    public String getGradeLevel() {
-//        // Determine the grade level of the student based on numberOfCredits
-//    }
+    public String getGradeLevel() {
+        // Determine the grade level of the student based on numberOfCredits
+        if (numberOfCredits <= 29){
+            return "Freshman";
+        } else if (numberOfCredits <= 59){
+            return "Sophomore";
+        } else if (numberOfCredits <= 89){
+            return "Junior";
+        } else {
+            return "Senior";
+        }
+
+    }
 
     // TODO: Complete the addGrade method.
     public void addGrade(int courseCredits, double grade) {
         // Update the appropriate fields: numberOfCredits, gpa
+        double totalQualityScore = this.gpa * this.numberOfCredits;
+        totalQualityScore += courseCredits * grade;
+        this.numberOfCredits += courseCredits;
+        this.gpa += totalQualityScore/this.numberOfCredits;
     }
 
     // TODO: Add your custom 'toString' method here. Make sure it returns a well-formatted String rather
     //  than just the class fields.
 
+    @Override
+    public String toString(){
+
+        String studentReport = name + "(" + studentId + ") has " + numberOfCredits+ " credits, with a GPA: "
+                + gpa + ". Which makes her a " + getGradeLevel();
+        return studentReport;
+    }
+
     // TODO: Add your custom 'equals' method here. Consider which fields should match in order to call two
     //  Student objects equal.
+
+    @Override
+    public boolean equals(Object toBeCompared){
+        if(toBeCompared == this){
+            return true;
+        }
+        if(toBeCompared == null){
+            return false;
+        }
+        if(toBeCompared.getClass() != getClass()){
+            return false;
+        }
+        Student theStudent = (Student) toBeCompared;
+        return theStudent.getStudentId() == getStudentId();
+    }
+
 
     public String getName() {
         return name;
