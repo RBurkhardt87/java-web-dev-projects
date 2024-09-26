47b4a84174632228b9efe20274f4d38b87cf6a81
Rachel Burkhardt <r.burkhardt.ibclc@gmail.com>
2024-09-26 18:32:13 -0500

    Still need to do the bonus in the exercise

--- interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
@@ -1,6 +1,7 @@
 package org.launchcode;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,9 +9,39 @@ public class Main {
         ArrayList<Flavor> flavors = menu.getFlavors();
         ArrayList<Cone> cones = menu.getCones();
 
+        Comparator comparator = new FlavorComparator();
+
+
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name' field.
+        System.out.println("BEFORE: ");
+        for (Flavor flavor : flavors){
+            System.out.println(flavor.getName());
+        }
+
+
+
+        flavors.sort(comparator);
+        //flavors.sort(new FlavorComparator());    <---- this is doing the same thing as line 12 and 16 together
+
+        System.out.println("\nAFTER:");
+        for (Flavor flavor : flavors){
+            System.out.println(flavor.getName());
+        }
 
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost' field.
+        System.out.println("\nBEFORE: ");
+        for (Cone cone : cones) {
+            System.out.println(cone.getCost());
+        }
+
+        //TODO: NEED TO CREATE AN INSTANCE OF THE COMPARATOR CLASS TO BE ABLE TO USE IT
+        cones.sort(new ConeComparator());
+
+        System.out.println("\nAFTER: ");
+        for (Cone cone : cones) {
+            System.out.println(cone.getCost());
+        }
+
 
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
     }
